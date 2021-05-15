47106b51dbe052f3baa9300388cf3f913e07beff
GabriellavdK <81296778+GabriellavdK@users.noreply.github.com>
2021-05-15 22:03:57 +0200

    Opdracht 25 geupload

---
+++ script.js
@@ -0,0 +1,26 @@
+// running the code below does not work because the function is not called. 
+const makePizza = function() {
+    console.log("Maak Pizzadeeg")
+    console.log("Vorm Pizzadeeg in een pizza")
+    console.log("Smeer Pizza in met tomatensaus")
+    console.log("Voeg jouw favoriete ingredienten toe")
+    console.log("Doe de Pizza in de over voor 15 min")
+};
+
+//below we call the function for 3 times
+makePizza();
+makePizza();
+
+// functie die uitlegt hoe je Sushi maakt
+const makeSushi = function() {
+    console.log("Kook rijst")
+    console.log("Rol een zeewierblad uit en beleg deze met gekookte rijst")
+    console.log("Voeg de vulling toe, bijvoorbeeld komkommer of tonijn")
+    console.log("Rol het zeewierblad met vulling op")
+    console.log("Snij de rol in stukjes van circa 2,5cm")
+};
+
+// Oproepen van de makeSushi functie. 
+makeSushi();
+makeSushi();
+makeSushi();
